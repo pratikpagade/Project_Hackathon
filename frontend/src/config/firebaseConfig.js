1e66a7a296308b047404942326b4e74682ae4fe2
Pratik Pagade <pratik.pagade@sjsu.edu>
2020-02-12 18:47:13 -0800

    Update firebaseConfig.js

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCvVM8QWicNyhNCSJCjvv-O463bG2s_Xwg",
-  authDomain: "openhack-403f7.firebaseapp.com",
-  databaseURL: "https://openhack-403f7.firebaseio.com",
-  projectId: "openhack-403f7",
-  storageBucket: "openhack-403f7.appspot.com",
-  messagingSenderId: "706489530077"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: ",
+  messagingSenderId: ""
 };
 const fire = firebase.initializeApp(config);
 
